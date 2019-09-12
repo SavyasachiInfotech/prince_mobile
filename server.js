5f94073b965b751bedc8b50517c55877216ee77b
parthDhankecha <pmd3290@gmail.com>
2019-09-13 03:48:47 +0530

    df

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
-const port = 3000;
+const port = 4200;
 const api = require("./server/api");
 var compression = require("compression");
 require("dotenv").config();
