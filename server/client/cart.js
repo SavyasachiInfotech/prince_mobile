b38398e85c162618c7ea06663a09c3805a15a000
parthDhankecha <pmd3290@gmail.com>
2019-10-12 07:40:35 +0530

    Cart cahnges

--- server/client/cart.js
+++ server/client/cart.js
@@ -28,14 +28,107 @@ function verifyToken(req, res, next) {
   next();
 }
 
-router.post(
-  "/add-cart",
-  [
-    check("variant_id").isNumeric(),
-    check("quantity").isNumeric(),
-    check("mobile_required").isBoolean(),
-    check("mobile_id").isArray()
-  ],
+router.post("/add-cart", verifyToken, (req, res) => {
+  let cart = req.body.cart;
+  let sql =
+    "insert into cart(cart_id,variant_id,quantity,mobile_required,mobile_id) values";
+  for (let i = 0; i < cart.length; i++) {
+    if (
+      isNaN(cart[i].variant_id) ||
+      isNaN(cart[i].quantity) ||
+      isNaN(cart[i].mobile_id) ||
+      isNaN(cart[i].mobile_required)
+    ) {
+      res.json(200).json({ status: "0", message: "Enter valid data" });
+      break;
+    } else {
+      if (i == cart.length - 1) {
+        sql +=
+          "(" +
+          req.userId +
+          "," +
+          cart[i].variant_id +
+          "," +
+          cart[i].quantity +
+          "," +
+          cart[i].mobile_required +
+          "," +
+          cart[i].mobile_id +
+          ");";
+      } else {
+        sql +=
+          "(" +
+          req.userId +
+          "," +
+          cart[i].variant_id +
+          "," +
+          cart[i].quantity +
+          "," +
+          cart[i].mobile_required +
+          "," +
+          cart[i].mobile_id +
+          "),";
+      }
+    }
+  }
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(200).json({
+        status: "0",
+        message: "Cart is not added. Please try again later."
+      });
+    } else {
+      res.status(200).json({
+        status: "1",
+        message: "Cart is added successfully."
+      });
+    }
+  });
+});
+
+router.put(
+  "/update-quantity",
+  [check("item_id").isNumeric(), check("quantity").isNumeric()],
+  verifyToken,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(200).json({
+        status: process.env.ERROR,
+        message: "Invalid Input Found",
+        errors: errors.array()
+      });
+    } else {
+      let data = req.body;
+      let sql =
+        "update cart set quantity=" +
+        data.quantity +
+        " where item_id=" +
+        data.item_id +
+        " and cart_id=" +
+        req.userId;
+      con.query(sql, (err, result) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(200)
+            .json({ status: "0", message: "Provide valid cart data" });
+        } else {
+          res.status(200).json({
+            status: "1",
+            message: "Cart's quantity updated successfully."
+          });
+        }
+      });
+    }
+  }
+);
+
+router.delete(
+  "/remove-cart-item/:item_id",
+  [param("item_id").isNumeric()],
   verifyToken,
   (req, res) => {
     const errors = validationResult(req);
@@ -46,51 +139,19 @@ router.post(
         errors: errors.array()
       });
     } else {
-      let cart = req.body;
       let sql =
-        "insert into cart(cart_id,variant_id,quantity,mobile_required,mobile_id) values";
-      for (let i = 0; i < cart.length; i++) {
-        if (i == cart.length - 1) {
-          sql +=
-            "(" +
-            req.userId +
-            "," +
-            cart.variant_id +
-            "," +
-            cart.quantity +
-            "," +
-            cart.mobile_required +
-            "," +
-            cart.mobile_id +
-            ");";
-        } else {
-          sql +=
-            "(" +
-            req.userId +
-            "," +
-            cart.variant_id +
-            "," +
-            cart.quantity +
-            "," +
-            cart.mobile_required +
-            "," +
-            cart.mobile_id +
-            "),";
-        }
-      }
-
+        "delete from cart where item_id=" +
+        req.params.item_id +
+        " and cart_id=" +
+        req.userId;
       con.query(sql, (err, result) => {
         if (err) {
           console.log(err);
-          res.status(200).json({
-            status: "0",
-            message: "Cart is not added. Please try again later."
-          });
+          res.status(200).json({ status: "0", message: "Provide valid cart" });
         } else {
-          res.status(200).json({
-            status: "1",
-            message: "Cart is added successfully."
-          });
+          res
+            .status(200)
+            .json({ status: "1", message: "Cart item removed successfully." });
         }
       });
     }
@@ -98,8 +159,38 @@ router.post(
 );
 
 router.get("/get-cart-detail", verifyToken, (req, res) => {
-  let sql = "select * from cart where cart_id=" + req.userId;
-  con.query(sql, (err, result) => {});
+  let sql =
+    "select c.item_id,c.variant_id,v.name,v.list_image,v.price,v.discount,t.tax,c.mobile_required,m.model_name from cart c, product_variant v,tax t,mobile_models m where c.cart_id=" +
+    req.userId +
+    " and  c.variant_id=v.variant_id and v.tax_id=t.tax_id and c.mobile_id=m.model_id";
+  con.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(200).json({ status: "0", message: "Enter valid user token" });
+    } else {
+      let image;
+      for (let i = 0; i < result.length; i++) {
+        result[i].mrp =
+          result[i].price + (result[i].price * result[i].discount) / 100;
+        image = JSON.parse(result[i].list_image);
+        if (image.length > 0) {
+          result[i].list_image =
+            process.env.LISTIMAGE + result[i].list_image[0];
+        } else {
+          result[i].list_image = "";
+        }
+      }
+      json = JSON.stringify(result);
+      result = JSON.parse(json, (key, val) =>
+        typeof val !== "object" && val !== null ? String(val) : val
+      );
+      res.status(200).json({
+        status: "1",
+        message: "Getting Cart Details Successfully.",
+        data: result
+      });
+    }
+  });
 });
 
 module.exports = router;
