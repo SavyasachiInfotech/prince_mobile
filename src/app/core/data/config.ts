1b7e4ffa8bd3ca46e3b6ac7da687ae7f6ed3e301
parth <pmd3290@gmail.com>
2019-11-23 19:43:11 +0530

    admin panel chage

--- src/app/core/data/config.ts
+++ src/app/core/data/config.ts
@@ -1,13 +1,13 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export class Config {
-  // public apiBaseUrl = "http://52.66.237.4:3000/api/";
-  // public clientBaseUrl="http://52.66.237.4:3000/";
-  // public imageUrl="http://52.66.237.4:3000/list-image/"
+  public apiBaseUrl = "http://52.66.237.4:3000/api/";
+  public clientBaseUrl="http://52.66.237.4:3000/";
+  public imageUrl="http://52.66.237.4:3000/list-image/"
 
-  public apiBaseUrl = "http://localhost:3000/api/";
-  public clientBaseUrl="http://localhost:3000/";
-  public imageUrl="http://localhost:3000/list-image/"
+  // public apiBaseUrl = "http://localhost:3000/api/";
+  // public clientBaseUrl="http://localhost:3000/";
+  // public imageUrl="http://localhost:3000/list-image/"
   public displayLimit = 10;
   public err = "No response found from server. Please try again later.";
 
