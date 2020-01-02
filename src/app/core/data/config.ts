f8f854871021347b33fe275e6bd7f36501144bdd
ParthDhankecha <pmd3290@gmail.com>
2020-01-02 21:24:50 +0530

    Admin panel build

--- src/app/core/data/config.ts
+++ src/app/core/data/config.ts
@@ -1,19 +1,19 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export class Config {
-  // public apiBaseUrl =
-  //   "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/api/";
-  // public clientBaseUrl =
-  //   "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/";
-  // public imageUrl =
-  //   "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/list-image/";
-  // public thumbnailUrl =
-  //   "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/thumbnail/";
+  public apiBaseUrl =
+    "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/api/";
+  public clientBaseUrl =
+    "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/";
+  public imageUrl =
+    "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/list-image/";
+  public thumbnailUrl =
+    "http://ec2-13-235-241-35.ap-south-1.compute.amazonaws.com:3000/thumbnail/";
 
-  public apiBaseUrl = "http://192.168.0.103:3000/api/";
-  public clientBaseUrl = "http://192.168.0.103:3000/";
-  public imageUrl = "http://192.168.0.103:3000/list-image/";
-  public thumbnailUrl = "http://192.168.0.103:3000/thumbnail/";
+  // public apiBaseUrl = "http://192.168.0.103:3000/api/";
+  // public clientBaseUrl = "http://192.168.0.103:3000/";
+  // public imageUrl = "http://192.168.0.103:3000/list-image/";
+  // public thumbnailUrl = "http://192.168.0.103:3000/thumbnail/";
   public displayLimit = 10;
   public err = "No response found from server. Please try again later.";
 
