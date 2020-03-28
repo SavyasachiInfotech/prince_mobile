548b20329b32ca3958933272f0847c19df801599
ParthDhankecha <pmd3290@gmail.com>
2020-03-28 11:56:50 +0530

    Changes in search filter

--- server/client/product.js
+++ server/client/product.js
@@ -26,15 +26,15 @@ router.post(
       let sql =
         "select distinct(v.variant_id),v.name,v.price,v.discount,t.tax,v.list_image,v.product_id from product p,product_variant v,tax t,category c where t.tax_id=v.tax_id and p.product_id=v.product_id and p.is_display=1 and v.parent=1 and p.category_id=c.category_id  and (c.name like '%" +
         search +
-        "%' or p.description like '%" +
-        search +
-        "%' or v.name like '%" +
-        search +
-        "%') or v.variant_id in (select distinct(vm.variant_id) from variant_mobile vm, mobile_models m where( m.model_name like '%" +
+        "%'  or v.variant_id in (select distinct(vm.variant_id) from variant_mobile vm, mobile_models m where( m.model_name like '%" +
         search +
         "%' " +
         mobileSearch +
-        " ) and vm.mobile_id=m.model_id ) " +
+        " ) and vm.mobile_id=m.model_id )  or p.description like '%" +
+        search +
+        "%' or v.name like '%" +
+        search +
+        "%') " +
         descriptionSearch +
         " " +
         variantSearch +
