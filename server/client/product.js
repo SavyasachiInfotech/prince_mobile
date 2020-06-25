2a537152e29376c62a1b4c2515fe04d7af4a880d
ParthDhankecha <pmd3290@gmail.com>
2020-06-25 19:02:09 +0530

    Changes of specification

--- server/client/product.js
+++ server/client/product.js
@@ -380,6 +380,7 @@ router.post(
                   "select specification_key,specification_value,variant_id from product_specification where variant_id=" +
                   id;
                 con.query(sql, (err, specifications) => {
+                  console.log(specifications);
                   if (err) {
                     console.log(err);
                     res
