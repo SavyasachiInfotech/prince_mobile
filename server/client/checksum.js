2a9d65935ec43024a076cbdf3019519fc609fd82
ParthDhankecha <pmd3290@gmail.com>
2020-12-10 11:48:31 +0530

    Change of the theme

--- server/client/checksum.js
+++ server/client/checksum.js
@@ -119,6 +119,51 @@ router.post(
                                 "\n"
                               );
                               console.log(paramarray)
+
+                              const https = require('https');
+
+                              let paytmData = {
+                                "body": paramarray,
+                                "head": {
+                                  "signature": checksum
+                                }
+                              };
+
+                              var post_data = JSON.stringify(paytmData);
+
+                              var options = {
+
+                                /* for Staging */
+                                hostname: 'securegw-stage.paytm.in',
+
+                                /* for Production */
+                                // hostname: 'securegw.paytm.in',
+
+                                port: 443,
+                                path: `/theia/api/v1/initiateTransaction?mid=${paramarray.mid}&orderId=${paramarray.orderId}`,
+                                method: 'POST',
+                                headers: {
+                                  'Content-Type': 'application/json',
+                                  'Content-Length': post_data.length
+                                }
+                              };
+
+                              var response = "";
+                              var post_req = https.request(options, function (post_res) {
+                                post_res.on('data', function (chunk) {
+                                  response += chunk;
+                                });
+
+                                post_res.on('end', function () {
+                                  console.log('Response: ', response);
+                                });
+                              });
+
+                              post_req.write(post_data);
+                              post_req.end();
+
+
+
                               axios.post(`${process.env.CREATE_CHECKSUM_URL.replace("{mid}", paramarray.mid).replace("{orderId}", paramarray.orderId)}`, {
                                 "body": paramarray,
                                 "head": {
